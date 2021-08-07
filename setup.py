399d4bc906e948dc641838ce2c5bf3abbfa4f97f
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-08-07 18:18:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="PyYoutube",
+    version="0.0.1",
+    author="mrlokaman",
+    author_email="ln0technical@gmail.com",
+    description="Python library Get YouTube Video Data",
+    license="MIT",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    
+)
