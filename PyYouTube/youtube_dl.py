ab2b5ffa39ba9fe6555c768edc99caccd3dd6edd
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-09-07 13:11:02 +0530

    Update youtube_dl.py

--- PyYouTube/youtube_dl.py
+++ PyYouTube/youtube_dl.py
@@ -3,6 +3,33 @@
 All rights reserved by LN Technical
 
 """
+
+"""
+MIT License
+
+Copyright (c) 2021 Lntechnical
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+
 # PyYouTube is optionally depends on youtube-dl 
 
 import youtube_dl
@@ -38,10 +65,7 @@ class ytdl:
 		  	if tag[0] == itag:
 		  		url_ =  tag[1]
 		  		url.append(url_)
-		  	else:
-		  		url_ =  'This tag not avilable'
-		  		url.append(url_)
-		  if url[0] == 'This tag not avilable' :
-		  	raise Error(url[0])
+		  if url:
+		  	return url[0]		  	
 		  else:
-		  	return url[0]				  			 	  	 						 	  	 				
+		  	raise Error('this tag not avilable')			  			 	  	 						 	  	 				
