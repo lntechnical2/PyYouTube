3426f3cb29dcdf435acf6e1f4d79d91af39ec936
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-09 18:01:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    install_requires = ["youtube_dl"],
+    install_requires = read("requirements.txt"),
     python_requires=">=3.6"
 )
