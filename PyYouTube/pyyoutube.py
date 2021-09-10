284f0da7c511e13065a93b5e287ed66229da1f5e
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-09-10 17:39:04 +0530

    Update pyyoutube.py

--- PyYouTube/pyyoutube.py
+++ PyYouTube/pyyoutube.py
@@ -238,41 +238,4 @@ class Data:
       	 
       	return json.dumps(DATA,indent = 2)
       	
-     
-      	
-
-	
-"""
-
-Search Youtube Video 
-
-Example:
-	
-from pyyoutube import Search
-
-yt = Search("ln technical" , 1)
-
-print(yt.videos)
-
-OUTPUT :
-	['https://youtu.be/{11}']
-"""		
-			
-					
-class Search:
-	
-	def __init__(self,keywords:str , limit=15):
-		
-		search_keyword=keywords.replace(" ", "+")
-		html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-		self.limit= limit
-		self.source = html.read().decode()
-	
-	def videos(self):
-		video_ids = re.findall(r"watch\?v=(\S{11})", self.source)[:self.limit]
-		video_link = []
-		for i in video_ids:
-			id = 'https://youtu.be/'+i
-			video_link.append(id)
-		return video_link
-		     	
+    
